13a0e6830b6bf9598c9b92469edfadd22d52d9b0
Marcus <marcus.carter@rogers.com>
2025-12-27 17:29:34 -0500

    ImGui Docking and Viewports

--- BallisticEditor/ImGuiLayer.cpp
+++ BallisticEditor/ImGuiLayer.cpp
@@ -59,12 +59,45 @@ namespace ballistic
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
+        const ImGuiViewport* viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->Pos);
+        ImGui::SetNextWindowSize(viewport->Size);
+        ImGui::SetNextWindowViewport(viewport->ID);
+        ImGuiWindowFlags window_flags =
+            ImGuiWindowFlags_NoDocking |
+            ImGuiWindowFlags_NoTitleBar |
+            ImGuiWindowFlags_NoCollapse |
+            ImGuiWindowFlags_NoResize |
+            ImGuiWindowFlags_NoMove |
+            ImGuiWindowFlags_NoBringToFrontOnFocus |
+            ImGuiWindowFlags_NoNavFocus |
+            ImGuiWindowFlags_MenuBar;
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("dockspace", nullptr, window_flags);
+        ImGuiID dockspace_id = ImGui::GetID("DockSpace");
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_None);
+        ImGui::PopStyleVar(3);
+        ImGui::End();
+
         ImGui::Begin("Demo Panel");
         ImGui::Text("Hello from ImGuiLayer!");
         ImGui::End();
 
+        // update all panels
+
+        ImGui::ShowDemoWindow();
+
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        auto io = ImGui::GetIO();
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+			GLFWwindow* backup = glfwGetCurrentContext();
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+			glfwMakeContextCurrent(backup);
+		}
     }
 
     void ImGuiLayer::OnEvent(IEvent& e) {
