c93e2ca235322e0cc5de552490da8ded469e76d4
Marcus Carter <marcoose408@gmail.com>
2026-06-22 01:47:39 -0400

    branding

--- platform/windows/main_editor.cpp
+++ platform/windows/main_editor.cpp
@@ -4,12 +4,18 @@
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-    AttachConsole(ATTACH_PARENT_PROCESS);
-
+    bool has_console = AttachConsole(ATTACH_PARENT_PROCESS) != 0;
+    
     FILE* dummy;
-    freopen_s(&dummy, "CONOUT$", "w", stdout);
-    freopen_s(&dummy, "CONOUT$", "w", stderr);
-    freopen_s(&dummy, "CONIN$", "r", stdin);
+    if (has_console) {
+        freopen_s(&dummy, "CONOUT$", "w", stdout);
+        freopen_s(&dummy, "CONOUT$", "w", stderr);
+        freopen_s(&dummy, "CONIN$", "r", stdin);
+    } else {
+        freopen_s(&dummy, "NUL", "w", stdout);
+        freopen_s(&dummy, "NUL", "w", stderr);
+        freopen_s(&dummy, "NUL", "r", stdin);
+    }
 
     ballistic::ApplicationCreateInfo info;
     info.window_title = L"Ballistic Editor";
